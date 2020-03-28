be1efaf4fede7445f1283f46502dd24a76c37ed6
Ashkan Zirakzadeh <ashkan.zirakzadeh@gmail.com>
2020-03-28 18:48:06 -0400

    initial push

---
+++ RiftBot/RiftBot/Keys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiftBot
+{
+    public static class Keys
+    {
+        public const string DISCORD_KEY = "";
+        public const string RIOT_KEY = "";
+    }
+}
---
+++ RiftBot/RiftBot/Program.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiftBot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            string bot_token = Keys.DISCORD_KEY;
+
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+            _client.Ready += () =>
+            {
+                Console.WriteLine("Bot is connected!");
+                return Task.CompletedTask;
+            };
+
+            await _client.LoginAsync(TokenType.Bot, bot_token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
