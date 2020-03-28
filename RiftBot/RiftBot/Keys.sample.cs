0e5dfaf39eee459d1467f9cf172c42f7f7aa55e0
Ashkan Zirakzadeh <ashkan.zirakzadeh@gmail.com>
2020-03-28 19:06:44 -0400

    Making keys.cs into sample file

--- RiftBot/RiftBot/Keys.sample.cs
+++ RiftBot/RiftBot/Keys.sample.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// You will need to create a Keys.cs with the following information filled out
 namespace RiftBot
 {
     public static class Keys
